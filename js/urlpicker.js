6aa04211b45795d448bc0f15406ea953b73e24d5
Paco Mtz <martinez@raidho.mx>
2015-08-08 17:19:42 -0500

    Lists in Popup showing elements from hardcoded array

--- js/urlpicker.js
+++ js/urlpicker.js
@@ -7,9 +7,69 @@
 // logURL();
 
 //THIS CODE WORKS
-chrome.tabs.query({'active': true}, function(tabs) {
-  chrome.tabs.update(tabs[0].id, {url: "http://raidho.mx/"});
-      var urlTitle = document.createElement("h2");
-      urlTitle.innerHTML = chrome.tabs[0].id;
-      console.log("Done");
-});
+chrome.tabs.query(
+  {'active': true}, function(tabs) {
+
+    var whitelist = ["codecademy.com",
+                    "coursera.com",
+                    "codewars.com",
+                    "codebabes.com"
+                    ];
+    var blacklist = ["facebook.com",
+                    "twitter.com",
+                    "buzzfeed.com",
+                    "9gag.com"
+                    ];
+    var url = window.location.hostname;
+    var x = 0;
+
+    //Agrega input a WHITELIST
+    function whitelistAdd() {
+            var wList = document.getElementById('section-linemove-2');
+
+            for (var i in whitelist) {
+              var wListItem = document.createElement("p");
+              wListItem.innerHTML = whitelist[i];
+              wList.appendChild(wListItem);
+            };
+    }
+
+    //Agrega input a BLACKLIST
+    function blacklistAdd() {
+            var bList = document.getElementById('section-linemove-1');
+
+            for (var i in blacklist) {
+              var bListItem = document.createElement("p");
+              bListItem.innerHTML = blacklist[i];
+              bList.appendChild(bListItem);
+            };
+    }
+
+    whitelistAdd();
+    blacklistAdd();
+
+    // //Comparación de URL con array BLACKLIST
+    // for (i = 0; i < blacklist.length; i++) {
+    //         if (blacklist[i] == url) {
+    //                 alert("Hello World");
+    //                 //url = whitelist[i];
+    //         };
+    // }
+
+
+
+    //var myUrl = window.location.hostname;
+
+// if (window.location.hostname == "facebook.com") {
+//   chrome.tabs.update(tabs[0].id, {url: "http://raidho.mx/"});
+// } else if (window.location.hostname !== "facebook.com") {
+//   console.log("BAD");
+//   //chrome.tabs.update(tabs[0].id, {url: "http://codecademy.com/"});
+// };
+
+      // var urlTitle = document.createElement("h2");
+      // urlTitle.innerHTML = chrome.tabs[0].id;
+
+      // chrome.tabs.update(tabs[0].id, {url: "http://raidho.mx/"});
+    }
+);
